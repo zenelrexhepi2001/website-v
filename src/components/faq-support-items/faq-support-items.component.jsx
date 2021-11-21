7cadb5d409e1aac151040fe3b172273a00e15c78
zenel.rexhepi2001 <zenel.rexhepi2001@gmail.com>
2021-11-21 16:40:50 +0100

    PUSH

--- src/components/faq-support-items/faq-support-items.component.jsx
+++ src/components/faq-support-items/faq-support-items.component.jsx
@@ -1,82 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
-import  {DataAccordion} from './faq-support-data';
-import PictureWoman from '../../assets/images/picture.svg';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { DataAccordion } from "./faq-support-data";
+import PictureWoman from "../../assets/images/picture.svg";
 import AccordionItems from "../accordion-items/accordion-items.component";
 import CoustumButton from "../coustum-buttons/coustum-button.component";
 import OpenAccordion from "../../assets/images/open.svg";
-import { IconContext } from 'react-icons';
-import { FiPlus, FiMinus} from 'react-icons/fi';
-import ArrowRight from '../../assets/images/Arrow-right.svg';
+import { IconContext } from "react-icons";
+import { FiPlus, FiMinus } from "react-icons/fi";
+import ArrowRight from "../../assets/images/Arrow-right.svg";
 import Footer from "../footer/footer.component";
-
-
+import {AiOutlineArrowRight, AiOutlineDown} from 'react-icons/ai';
 
 const FaqSupportItems = () => {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked,setClicked] = useState(false);
+  useEffect(() => {
+    document.title = `Voltox FAQ Support`;
+  });
 
-
-    useEffect(() => {
-       document.title = `Voltox FAQ Support`
-   })
-
-    const ToggleAccordion = (index) => {
-        if(clicked === index) {
-            return setClicked(null);
-        }
-        setClicked(index);
+  const ToggleAccordion = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
     }
+    setClicked(index);
 
-        return (
-            <div className="faq-support">
-                <div className="container">
-                    <div className="faq-support-titles">
-                        <h3>Learn more</h3>
-                        <h1>Frequently Asked Questions</h1>
-                    </div>
-                    <div className="faq-support-parent">
-                        <div className="accordion">
-                            {
-                                DataAccordion.map((item,index) => {
-                                    return(
-                                        <div className="accordion-item">
-                                            <div className="accordion-elements"  onClick={() => ToggleAccordion(index)} key={index}>
-                                                <h1 className="accordion-title">{item.title}</h1>
-                                                <span>{clicked === index ? <FiMinus style={{fontSize: '25px'}} /> : <FiPlus style={{fontSize: '25px'}}  />}</span>
-                                            </div>
-                                            <div className="accordion-body">
-                                            {clicked === index ? (
-                                                <>
-                                                    <p>{item.content}</p>
-                                                </>
-                                            ) : null}
-                                            </div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className="faq-support-picture">
-                            <img src={PictureWoman} alt="picture"/>
-                        </div>
-                    </div>
-                    <div className="footer-faq-support">
-                        <h1>Still have a question?</h1>
-                        <p>
-                            If you cannot find your answer in our FAQ, you can always contact us.<br/>
-                            We will answer to you shortly!
-                        </p>
-                        <Link to='/contact-support'>
-                            Contact Support
-                        </Link>
-                    </div>
+      console.warn(index);
+  };
+
+  return (
+    <div className="faq-support">
+      <div className="container">
+        <div className="faq-support-titles">
+          <h3>Learn more</h3>
+          <h1>Frequently Asked Questions</h1>
+        </div>
+        <div className="faq-support-parent">
+          <div className="accordion">
+            {DataAccordion.map((item, index) => {
+              return (
+                <div className="accordion-item">
+                  <div
+                    className="accordion-elements"
+                    onClick={() => ToggleAccordion(index)}
+                    key={index}
+                  >
+                    <h1 className="accordion-title">{item.title}</h1>
+                    <span>
+                      {clicked === index ? (
+                        <FiMinus style={{ fontSize: "25px" }} />
+                      ) : (
+                        <FiPlus style={{ fontSize: "25px" }} />
+                      )}
+                    </span>
+                  </div>
+                  <div className="accordion-body">
+                    {clicked === index ? (
+                      <>
+                        <p>{item.content}</p>
+                      </>
+                    ) : null}
+                  </div>
                 </div>
-                <Footer/>
+              );
+            })}
+            <div className="footer-faq-support">
+              <h1>Still have a question?</h1>
+              <p>
+                If you cannot find your answer in our FAQ, you can always
+                contact us.
+                <br />
+                We will answer to you shortly!
+              </p>
+              <Link to="/contact-support">Contact Support<AiOutlineArrowRight/></Link>
             </div>
-        )
-}
+          </div>
 
+          <div className="faq-support-picture">
+            <img src={PictureWoman} alt="picture" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-
-export default  FaqSupportItems;
+export default FaqSupportItems;
