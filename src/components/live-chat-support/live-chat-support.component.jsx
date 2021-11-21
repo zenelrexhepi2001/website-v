8905a562a8649a7a1cd6ee49adbe36d981a866fa
zenel.rexhepi2001 <zenel.rexhepi2001@gmail.com>
2021-11-21 11:55:09 +0100

    PUSH

--- src/components/live-chat-support/live-chat-support.component.jsx
+++ src/components/live-chat-support/live-chat-support.component.jsx
@@ -1,63 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { DataLivechatsupport } from "./live-chat-support.data";
 import CoustumButton from "../coustum-buttons/coustum-button.component";
-import LiveChatSupportItem from "../live-chat-support-items/live-chat-support-item.component";
-import ItSupportImage from '../../assets/images/IT Support_Monochromatic.svg';
+import ItSupportImage from "../../assets/images/IT Support_Monochromatic.svg";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
+const LiveChatSupportPage = () => {
+  const [clickedAccordion, setClickedAccordion] = useState(false);
 
-class liveChatSupportPage extends React.Component {
-    constructor() {
-        super();
+  useEffect(() => {
+    document.title = `Voltox Live chat support`;
+  });
 
-        this.state = {
-            LiveChatdata: [
-                {
-                    id: 1,
-                    title: 'Select the Topic',
-                },
-                {
-                    id: 2,
-                    title: 'Select the issue',
-                },
-                {
-                    id: 3,
-                    title: 'Select the support type',
-                }
-            ]
-        }
+  const AccordionToggle = (index) => {
+    if (clickedAccordion === index) {
+      return setClickedAccordion(null);
     }
-    componentDidMount() {
-        document.title = `Voltox Live chat support`
-    }
-    render() {
-        return(
-            <div className="faq-support">
-                <div className="container">
-                    <div className="faq-support-titles">
-                        <h3>Read more</h3>
-                        <h1>Live chat support</h1>
-                    </div>
-                        <div className="live-chat-support-titles">
-                            <h1>You must be signed in</h1>
-                            <p>If you are not signed in, please do, so you can live chat with
-                                our advisors and we know who we're talking to!</p>
-                        </div>
-                    <div className="live-chat-support-elements">
-                        <div className="live-chat-support-dropdowns">
-                            {
-                                this.state.LiveChatdata.map((getdataLiveSupport) => (
-                                    <LiveChatSupportItem key={getdataLiveSupport.id} getdataLiveSupport={getdataLiveSupport}/>
-                                ))
-                            }
-                            <CoustumButton type="button" className="btn btn-primary">Request</CoustumButton>
-                        </div>
-                        <div className="live-chat-support-image">
-                            <img alt="it-support" src={ItSupportImage}/>
-                        </div>
-                    </div>
+    setClickedAccordion(index);
+  };
+
+  return (
+    <div className="faq-support">
+      <div className="container">
+        <div className="faq-support-titles">
+          <h3>Read more</h3>
+          <h1>Live chat support</h1>
+        </div>
+        <div className="live-chat-support-titles">
+          <h1>You must be signed in</h1>
+          <p>
+            If you are not signed in, please do, so you can live chat with our
+            advisors and we know who we're talking to!
+          </p>
+        </div>
+        <div className="live-chat-support-elements">
+          <div className="live-chat-support-dropdowns">
+            {DataLivechatsupport.map((data, index) => (
+              <div
+                className="live-chat-dropdown"
+                onClick={() => AccordionToggle(index)}
+                key={index}
+              >
+                <div className="live-chat-flex-item">
+                  <h1>{data.title}</h1>
+                  <CoustumButton type="button" className="btn btn-light">
+                    {clickedAccordion === index ? (
+                      <AiOutlineUp />
+                    ) : (
+                      <AiOutlineDown />
+                    )}
+                  </CoustumButton>
                 </div>
-            </div>
-        )
-    }
-}
+                <div className="live-chat-support-dropdown">
+                  {clickedAccordion === index ? (
+                    <p>{data.description}</p>
+                  ) : null}
+                </div>
+              </div>
+            ))}
 
-export default  liveChatSupportPage;
+            <CoustumButton type="button" className="btn btn-primary">
+              Request
+            </CoustumButton>
+          </div>
+          <div className="live-chat-support-image">
+            <img alt="it-support" src={ItSupportImage} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LiveChatSupportPage;
---
+++ src/components/live-chat-support/live-chat-support.data.js
@@ -0,0 +1,17 @@
+export const DataLivechatsupport = [
+  {
+    title: "Select the Topic",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy",
+  },
+  {
+    title: "Select the issue",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy",
+  },
+  {
+    title: "Select the support type",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy",
+  },
+];
